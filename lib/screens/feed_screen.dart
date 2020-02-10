a42e4b3e2973e71dd483fb0374eb9ea4d3048305
wenksi <silvan.wenk@gmail.com>
2020-02-10 07:59:51 +0100

    Convert FeedScreen to Stateful Widget

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 
-class FeedScreen extends StatelessWidget {
+class FeedScreen extends StatefulWidget {
+  @override
+  _FeedScreenState createState() => _FeedScreenState();
+}
+
+class _FeedScreenState extends State<FeedScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Salto'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          )
+        ],
       ),
       body: Text('Content'),
     );
