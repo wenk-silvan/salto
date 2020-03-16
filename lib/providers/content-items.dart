471e8546421ac1fa383e0dd8c793e525b86e12e0
wenksi <silvan.wenk@gmail.com>
2020-03-16 10:18:51 +0100

    Fetch sample data from firebase

--- lib/providers/content-items.dart
+++ lib/providers/content-items.dart
@@ -1,174 +1,78 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:salto/models/user.dart';
+import 'package:salto/providers/users.dart';
 
 import '../models/comment.dart';
 import '../models/content-item.dart';
+import 'package:http/http.dart' as http;
 
 class ContentItems with ChangeNotifier {
-  List<ContentItem> _items = [
-    ContentItem(
-        id: '70',
-        title: 'Fleek\'s Backflip',
-        description: 'Backflip in den Sand',
-        mediaUrl:
-            'https://upload.wikimedia.org/wikipedia/commons/b/b3/Backflip.jpg',
-        /*timestamp: DateTime(2019, 1, 1, 12, 0, 0),*/
-        userId: 'ed4f546q',
-        comments: [
-          Comment(
-            id: "150",
-            text:
-                'Sick! Yes, this comment is supposed to be very long so it wraps around multiple lines to test whether the layout stays anchored...',
-            userId: '5a4f546e',
-            /*timestamp: DateTime(2019, 1, 1, 12, 1, 30),*/
-          ),
-          Comment(
-            id: "151",
-            text: 'Nice!',
-            userId: '5a4f546e',
-            /*timestamp: DateTime(2019, 1, 1, 12, 2, 30),*/
-          ),
-        ],
-        likes: [
-          'ed4f546q',
-          '5a4f546e',
-        ]),
-    ContentItem(
-        id: '73',
-        title: 'Its tutorial time.',
-        description: 'Backflip in den Sand',
-        mediaUrl:
-            'https://www.wikihow.com/images/thumb/1/18/Do-Trampoline-Tricks-Step-2.jpg/aid204008-v4-728px-Do-Trampoline-Tricks-Step-2.jpg',
-        /*timestamp: DateTime(2019, 1, 1, 12, 0, 0),*/
-        userId: '5a4f546e',
-        comments: [
-          Comment(
-            id: "156",
-            text:
-                'Sick! Yes, this comment is supposed to be very long so it wraps around multiple lines to test whether the layout stays anchored...',
-            userId: '5a4f546e',
-            /*timestamp: DateTime(2019, 1, 1, 12, 1, 30),*/
-          ),
-          Comment(
-            id: "157",
-            text: 'Nice!',
-            userId: '5a4f546e',
-            /*timestamp: DateTime(2019, 1, 1, 12, 2, 30),*/
-          ),
-        ],
-        likes: [
-          'ed4f546q',
-          '5a4f546e',
-        ]),
-    ContentItem(
-        id: '71',
-        title: 'Another trick',
-        description: 'Backflip in den Sand',
-        mediaUrl: 'https://i.ytimg.com/vi/qu1deqZJ8_A/maxresdefault.jpg',
-        /*timestamp: DateTime(2019, 1, 1, 12, 0, 0),*/
-        userId: 'ed4f546q',
-        comments: [
-          Comment(
-            id: "152",
-            text:
-                'Sick! Yes, this comment is supposed to be very long so it wraps around multiple lines to test whether the layout stays anchored...',
-            userId: '5a4f546e',
-            /*timestamp: DateTime(2019, 1, 1, 12, 1, 30),*/
-          ),
-          Comment(
-            id: "153",
-            text: 'Nice!',
-            userId: '5a4f546e',
-            /*timestamp: DateTime(2019, 1, 1, 12, 2, 30),*/
-          ),
-        ],
-        likes: [
-          'ed4f546q',
-          '5a4f546e',
-        ]),
-    ContentItem(
-        id: '72',
-        title: 'Yet another tramp salto',
-        description: 'Backflip in den Sand',
-        mediaUrl:
-            'http://realbuzz4.s3.amazonaws.com/photo_field_photos/600x450/d7329c10a660bd47cb9ac8fe75a3f05c677b.jpg',
-        /*timestamp: DateTime(2019, 1, 1, 12, 0, 0),*/
-        userId: 'ed4f546q',
-        comments: [
-          Comment(
-            id: "154",
-            text:
-                'Sick! Yes, this comment is supposed to be very long so it wraps around multiple lines to test whether the layout stays anchored...',
-            userId: '5a4f546e',
-            /*timestamp: DateTime(2019, 1, 1, 12, 1, 30),*/
-          ),
-          Comment(
-            id: "155",
-            text: 'Nice!',
-            userId: '5a4f546e',
-            /*timestamp: DateTime(2019, 1, 1, 12, 2, 30),*/
-          ),
-        ],
-        likes: [
-          'ed4f546q',
-          '5a4f546e',
-        ]),
-    ContentItem(
-        id: '74',
-        title: 'Another tutorial baby',
-        description: 'Backflip in den Sand',
-        mediaUrl:
-            'https://www.wikihow.com/images/thumb/6/65/Do-Trampoline-Tricks-Step-4.jpg/aid204008-v4-728px-Do-Trampoline-Tricks-Step-4.jpg',
-        /*timestamp: DateTime(2019, 1, 1, 12, 0, 0),*/
-        userId: '5a4f546e',
-        comments: [
-          Comment(
-            id: "158",
-            text:
-                'Sick! Yes, this comment is supposed to be very long so it wraps around multiple lines to test whether the layout stays anchored...',
-            userId: '5a4f546e',
-            /*timestamp: DateTime(2019, 1, 1, 12, 1, 30),*/
-          ),
-          Comment(
-            id: "159",
-            text: 'Nice!',
-            userId: '5a4f546e',
-            /*timestamp: DateTime(2019, 1, 1, 12, 2, 30),*/
-          ),
-        ],
-        likes: [
-          'ed4f546q',
-          '5a4f546e',
-        ]),
-    ContentItem(
-        id: '75',
-        title: 'The Whackflip',
-        description: 'Backflip in den Sand',
-        mediaUrl: 'http://i.ytimg.com/vi/6qMYH9EOJUo/hqdefault.jpg',
-        /*timestamp: DateTime(2019, 1, 1, 12, 0, 0),*/
-        userId: 'ed4f546q',
-        comments: [
-          Comment(
-            id: "160",
-            text:
-                'Sick! Yes, this comment is supposed to be very long so it wraps around multiple lines to test whether the layout stays anchored...',
-            userId: '5a4f546e',
-            /*timestamp: DateTime(2019, 1, 1, 12, 1, 30),*/
-          ),
-          Comment(
-            id: "161",
-            text: 'Nice!',
-            userId: '5a4f546e',
-            /*timestamp: DateTime(2019, 1, 1, 12, 2, 30),*/
-          ),
-        ],
-        likes: [
-          'ed4f546q',
-          '5a4f546e',
-        ]),
-  ];
+  static const url = "https://salto-7fab8.firebaseio.com/";
+   List<String> _favoriteUserIds = [];
+
+  List<ContentItem> _favItems = [];
+  List<ContentItem> _items = [];
 
   List<ContentItem> get items {
     return this._items;
+  }
+
+  List<ContentItem> get favItems {
+    return this.getContentOfUsers(this._favoriteUserIds);
+  }
+
+  Future<void> addContent(ContentItem item) async {
+    final timestamp = DateTime.now();
+    final body = json.encode({
+      'title': item.title,
+      'description': item.description,
+      'mediaUrl': item.mediaUrl,
+      'dateTime': timestamp.toIso8601String(),
+      'userId': item.userId,
+      'comments': item.comments
+          .map((c) => {
+            'id': c.id,
+            'userId': c.userId,
+            'timestamp': c.timestamp.toIso8601String(),
+            'text': c.text,
+          }).toList(),
+      'likes': item.likes
+    });
+    final response = await http.post(
+      url + "/content.json",
+      body: body
+    );
+    this.notifyListeners();
+  }
+
+  Future<void> getContent(User user) async {
+    this._favoriteUserIds = user.follows;
+    final response = await http.get(url + 'content.json');
+    final List<ContentItem> loadedContent = [];
+    final extracted = json.decode(response.body) as Map<String, dynamic>;
+    if (extracted == null) return;
+    extracted.forEach((contentId, contentData) {
+      loadedContent.add(ContentItem(
+        id: contentId,
+        mediaUrl: contentData['mediaUrl'],
+        title: contentData['title'],
+        userId: contentData['userId'],
+        timestamp: DateTime.parse(contentData['dateTime']),
+        description: contentData['description'],
+        comments: (contentData['comments'] as List<dynamic>).map((comment) => Comment(
+          timestamp: DateTime.parse(comment['timestamp']),
+          userId: comment['userId'],
+          id: comment['id'],
+          text: comment['text'],
+        )).toList(),
+        likes: (contentData['likes'] as List<dynamic>).map((userId) => userId.toString()).toList(),
+      ));
+    });
+    this._items = loadedContent.toList();
+    print("Loaded content from database.");
+    this.notifyListeners();
   }
 
   List<String> getMediaByUserId(String userId) {
--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -3,8 +3,6 @@ import 'package:salto/components/feed_post.dart';
 import 'package:provider/provider.dart';
 import 'package:salto/models/user.dart';
 import 'package:salto/providers/content-items.dart';
-import 'package:salto/providers/users.dart';
-import '../models/content-item.dart';
 
 class FeedScreen extends StatefulWidget {
   final bool isFavorites;
@@ -17,21 +15,55 @@ class FeedScreen extends StatefulWidget {
 }
 
 class _FeedScreenState extends State<FeedScreen> {
+  /*@override
+  void initState() {
+    Future.delayed(Duration.zero).then((_) =>
+      Provider.of<ContentItems>(context).getContent());
+    super.initState();
+  }*/
+
   @override
   Widget build(BuildContext context) {
-    final contentData = Provider.of<ContentItems>(context);
+    /*final contentData = Provider.of<ContentItems>(context);
     List<ContentItem> content = widget.isFavorites
         ? contentData.getContentOfUsers(widget.currentUser.follows)
-        : contentData.items;
+        : contentData.items;*/
 
     return RefreshIndicator(
       onRefresh: () {
         //TODO: Fetch content
         return null;
       },
-      child: ListView.builder(
-        itemBuilder: (_, i) => FeedPost(content[i]),
-        itemCount: content.length,
+      child: FutureBuilder(
+        future: Provider.of<ContentItems>(context, listen: false)
+            .getContent(widget.currentUser),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            if (dataSnapshot.error != null) {
+              // ...
+              // Do error handling stuff
+              return Center(
+                child: Text('An error occurred!'),
+              );
+            } else {
+              return widget.isFavorites
+                  ? Consumer<ContentItems>(
+                      builder: (ctx, orderData, child) => ListView.builder(
+                        itemBuilder: (_, i) => FeedPost(orderData.favItems[i]),
+                        itemCount: orderData.favItems.length,
+                      ),
+                    )
+                  : Consumer<ContentItems>(
+                      builder: (ctx, orderData, child) => ListView.builder(
+                        itemBuilder: (_, i) => FeedPost(orderData.items[i]),
+                        itemCount: orderData.items.length,
+                      ),
+                    );
+            }
+          }
+        },
       ),
     );
   }
