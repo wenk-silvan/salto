8cc0a22b4e75ce7afe5d928b0cf8d90552711109
wenksi <silvan.wenk@gmail.com>
2020-03-29 20:55:18 +0200

    Change design of Auth Screen

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:salto/components/auth_actions.dart';
 
 class AuthScreen extends StatelessWidget {
@@ -8,31 +9,39 @@ class AuthScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            top: 50.0,
-            right: 10,
-            left: 10,
-            bottom: 10,
-          ),
-          child: Card(
-            child: Padding(
-              padding: EdgeInsets.all(8),
-              child: Column(
-                children: <Widget>[
-                  ListTile(
-                    title: Text(
-                      'Salto',
-                      style: TextStyle(
-                        fontSize: 40,
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/trampoline_feathers.jpg'),
+              fit: BoxFit.cover,
+            )
+        ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: 50.0,
+              right: 10,
+              left: 10,
+              bottom: 10,
+            ),
+            child: Card(
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  children: <Widget>[
+                    ListTile(
+                      title: Text(
+                        'Salto',
+                        style: TextStyle(
+                          fontSize: 40,
+                        ),
                       ),
+                      subtitle: Text('Freestyle Trampoline Community App'),
                     ),
-                    subtitle: Text('Freestyle Trampoline Community App'),
-                  ),
-                  SizedBox(height: deviceSize.height * 0.05),
-                  AuthActions(),
-                ],
+                    AuthActions(),
+                  ],
+                ),
               ),
             ),
           ),
