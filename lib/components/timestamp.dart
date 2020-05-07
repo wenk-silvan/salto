73c933514e44c673292528f8eefc28a55e18ae9c
troxlepa <troxlepa@student.ethz.ch>
2020-05-07 16:58:13 +0200

    timestamp, show ago if less than 24 hours

--- lib/components/timestamp.dart
+++ lib/components/timestamp.dart
@@ -8,7 +8,18 @@ class Timestamp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(DateFormat('dd.MM.yyyy').format(this.timestamp),
-        style: TextStyle(color: Colors.grey, fontSize: 12));
+    Duration delta = DateTime.now().difference(timestamp);
+    TextStyle style = TextStyle(color: Colors.grey, fontSize: 12);
+    if(delta.inDays > 0)
+      return Text(DateFormat('dd.MM.yyyy').format(this.timestamp),
+          style: style);
+    if(delta.inHours > 0)
+      return Text("${delta.inHours} ${delta.inHours == 1 ? "hour" : "hours"} ago",
+          style: style);
+    if(delta.inMinutes > 0)
+      return Text("${delta.inMinutes} ${delta.inMinutes == 1 ? "min" : "mins"} ago",
+          style: style);
+    return Text("just now",
+        style: style);
   }
 }
